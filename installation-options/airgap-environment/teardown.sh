fa9776ebe23948546606407e35e874e764e04537
Will McKinley <will.mckinley@solo.io>
2025-03-10 10:49:13 -0400

    Fix teardown script

--- installation-options/airgap-environment/teardown.sh
+++ installation-options/airgap-environment/teardown.sh
@@ -5,3 +5,7 @@ SCRIPT_DIR=$(dirname "$0")
 # Execute installation script from get-started
 $SCRIPT_DIR/../../get-started/uninstall.sh
 
+# Remove the k3d cluster
+k3d cluster delete gloo
+docker network rm k3d-cluster-network
+k3d registry delete myregistry.localhost
