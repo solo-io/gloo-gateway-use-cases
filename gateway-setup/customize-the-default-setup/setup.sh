47cb518a410505e898818ee766d35d9505e9c5de
Will McKinley <will.mckinley@solo.io>
2025-03-11 17:07:44 -0400

    added tests for customize gateway setup

---
+++ gateway-setup/customize-the-default-setup/setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# First, need to check for the existence of a license key.
+if [[ -z "${GLOO_GATEWAY_LICENSE_KEY}" ]]; then
+  echo "Please set the GLOO_GATEWAY_LICENSE_KEY environment variable."
+  exit 1
+fi
+
+GLOO_VERSION=1.18.5
+
+SCRIPT_DIR=$(dirname "$0")
+
+# Execute installation script from get-started
+$SCRIPT_DIR/../../get-started/install-ee-helm.sh
+
+echo "Creating a new GatewayParameters with label custom"
+kubectl apply -f- <<EOF
+apiVersion: gateway.gloo.solo.io/v1alpha1
+kind: GatewayParameters
+metadata:
+  name: custom-gw-params
+  namespace: gloo-system
+spec:
+  kube:
+    service:
+      type: NodePort
+      extraLabels:
+        gateway: custom
+    podTemplate:
+      extraLabels:
+        gateway: custom
+      securityContext:
+        fsGroup: 50000
+        runAsUser: 50000
+EOF
+
+echo "Creating a Gateway using the new GatewayParameters"
+kubectl apply -f- <<EOF
+kind: Gateway
+apiVersion: gateway.networking.k8s.io/v1
+metadata:
+  name: custom
+  namespace: gloo-system
+  annotations:
+    gateway.gloo.solo.io/gateway-parameters-name: "custom-gw-params"
+spec:
+  gatewayClassName: gloo-gateway
+  listeners:
+  - protocol: HTTP
+    port: 80
+    name: http
+    allowedRoutes:
+      namespaces:
+        from: All
+EOF
+
+echo "Completed setup.  Run tests via 'chainsaw test'"
---
+++ gateway-setup/customize-the-default-setup/teardown.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+
+# Execute installation script from get-started
+$SCRIPT_DIR/../../get-started/uninstall.sh
