dc842eb78542fc347dd8b6c0fb77abd1d239c553
Will McKinley <will.mckinley@solo.io>
2025-03-31 11:30:38 -0400

    Added optional bookinfo and corresponding tests

--- gateway-setup/deployment-patterns/ingress/ingress-to-ambient-mesh/setup.sh
+++ gateway-setup/deployment-patterns/ingress/ingress-to-ambient-mesh/setup.sh
@@ -56,4 +56,48 @@ echo "Setting up Gloo Gateway for ingress..."
 kubectl label ns gloo-system istio.io/dataplane-mode=ambient
 kubectl label ns httpbin istio.io/dataplane-mode=ambient
 
+# Step 3: Expose the Bookinfo sample app
+echo "Exposing the Bookinfo sample app..."
+kubectl create ns bookinfo
+kubectl label namespace bookinfo istio.io/dataplane-mode=ambient
+
+# deploy bookinfo application components for all versions
+kubectl -n bookinfo apply -f https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/platform/kube/bookinfo.yaml -l 'app'
+# deploy an updated product page with extra container utilities such as 'curl' and 'netcat'
+kubectl -n bookinfo apply -f https://raw.githubusercontent.com/solo-io/gloo-mesh-use-cases/main/policy-demo/productpage-with-curl.yaml
+# deploy all bookinfo service accounts
+kubectl -n bookinfo apply -f https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/platform/kube/bookinfo.yaml -l 'account'
+
+kubectl apply -n bookinfo -f- <<EOF
+apiVersion: gateway.networking.k8s.io/v1
+kind: HTTPRoute
+metadata:
+  name: bookinfo
+spec:
+  parentRefs:
+  - name: http
+    namespace: gloo-system
+  rules:
+  - matches:
+    - path:
+        type: Exact
+        value: /productpage
+    - path:
+        type: PathPrefix
+        value: /static
+    - path:
+        type: Exact
+        value: /login
+    - path:
+        type: Exact
+        value: /logout
+    - path:
+        type: PathPrefix
+        value: /api/v1/products
+    backendRefs:
+      - name: productpage
+        port: 9080
+EOF
+
+
 echo "Setup complete.  Run tests via 'chainsaw test'"
--- gateway-setup/deployment-patterns/ingress/ingress-to-ambient-mesh/teardown.sh
+++ gateway-setup/deployment-patterns/ingress/ingress-to-ambient-mesh/teardown.sh
@@ -2,6 +2,12 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
+kubectl delete httproute bookinfo -n bookinfo
+kubectl -n bookinfo delete -f https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/platform/kube/bookinfo.yaml -l 'account'
+kubectl -n bookinfo delete -f https://raw.githubusercontent.com/solo-io/gloo-mesh-use-cases/main/policy-demo/productpage-with-curl.yaml
+kubectl -n bookinfo delete -f https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/platform/kube/bookinfo.yaml -l 'app'
+kubectl delete ns bookinfo
+
 kubectl delete servicemeshcontroller managed-istio -n gloo-mesh
 
 sleep 30
